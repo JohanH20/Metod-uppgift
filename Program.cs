b066953f3412f3399db926acfcde39d3cf91d8f8
JohanH20 <54435441+JohanH20@users.noreply.github.com>
2019-09-20 09:28:52 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Metod_uppgift
+{
+    class Program
+    {
+            public static void Main()
+            {
+                double radie = 2.4;
+                Console.WriteLine("Cirkelns omkrets: " + CirkelOmkrets(radie));
+                Console.WriteLine("Cirkelns area: " + CirkelArea(radie));
+            }
+
+            static double CirkelOmkrets(double r)
+            {
+                double o = r * Math.PI;
+                return o;
+            }
+            static double CirkelArea(double r)
+            {
+            double a = r * r * Math.PI;
+            return a;
+            }
+        
+    }
+}
